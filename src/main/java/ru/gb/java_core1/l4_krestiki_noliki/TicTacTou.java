6cc4d92da1681a6f039cd739f7746c44131b079e
DAZZ <art.murtazov@gmail.com>
2022-04-03 13:07:12 +0300

    l4 DZ krestiki noliki

--- src/main/java/ru/gb/java_core1/l4_krestiki_noliki/TicTacTou.java
+++ src/main/java/ru/gb/java_core1/l4_krestiki_noliki/TicTacTou.java
@@ -23,11 +23,39 @@ public class TicTacTou {
     private static int roundCounter;
     private static int winLength;
 
+/*
+СХЕМА РАБОТЫ ПРОГРАММЫ:
+main(String[] args)
+startNewGame();
+chooseDot();
+playRound();
+initField(sizeX, sizeY);
+printField();
+
+humanFirst();	//цикл игры тут если игрок выбрал "Х", если нет то тут aiFirst()
+
+humanTurn();	//ход игрока
+isCellValid(int y, int x);  //проверяет возможность установки фишки в указанную ячейку
+printField();	//печать поля
+gameCheck(dotHuman);	//проверка победы игрока или аи через вызов метода checkWin()
+checkWin(dot) && dot == dotHuman;  //проверка победы игрока
+checkWin(dot) && dot == dotAi;	   //проверка победы аи
+checkDraw();	//проверка - заполненна ли карта, если да, то ничья
+
+aiTurn();
+isCellValid(int y, int x);
+printField();
+gameCheck(dotAi);
+checkWin(dot) && dot == dotHuman;
+checkWin(dot) && dot == dotAi;
+checkDraw();
 
 
+humanTurn();    //ход игрока итд......
+...
+aiTurn(); ...
 
-
-
+ */
 
 
     public static void main(String[] args) {
@@ -43,7 +71,6 @@ public class TicTacTou {
             System.out.printf("SCORE:     HUMAN     AI\n" +
                     "             %d       %d\n", scoreHuman, scoreAi);
 
-
             System.out.print("Want to play again ? (Y or N) >>> ");
             if(!scanner.next().toLowerCase(Locale.ROOT).equals("y")) {
                 System.out.println("BYE");
@@ -52,23 +79,23 @@ public class TicTacTou {
         }
     }
 
+    //выбор Х или 0
+    private static void chooseDot() {
+        System.out.print("Type 'X' to play with X and for '0' type anything >>> ");
+
+        if(scanner.next().toLowerCase(Locale.ROOT).equals("x")) {
+            dotHuman = DOT_X;
+            dotAi = DOT_0;
+        } else {
+            dotHuman = DOT_0;
+            dotAi = DOT_X;
+        }
+    }
+
     private static void playRound() {
         System.out.printf("Round %d start\n", ++roundCounter);
-
         int sizeX = -0;
         int sizeY;
-/*
-        do {
-            System.out.print("Введите размер поля по X: ");
-            sizeX = scanner.nextInt();
-            System.out.print("Введите размер поля по Y: ");
-            sizeY = scanner.nextInt();
-            System.out.print("Введите: 0 и нажмите Enter");
-
-        } while(!scanner.hasNextInt());
-
-        initField(sizeX, sizeY);
-*/
         boolean check = true;
 
         while(check) {
@@ -86,14 +113,13 @@ public class TicTacTou {
                 check = true; //если вышло исключение, ставим обратно true, что бы опять вводить данные
             }
         }
-
         sizeY = sizeX;
         initField(sizeX, sizeY);
-
         //initField(3, 3);
 
         printField();
-        if(dotHuman == DOT_X) {
+
+        if(dotHuman == DOT_X) {     //тут ходит первый тот кто выбрал "Х"
             humanFirst();
         } else {
             aiFirst();
@@ -143,29 +169,23 @@ public class TicTacTou {
         return checkDraw();
     }
 
-    //выбор Х или 0
-    private static void chooseDot() {
-        System.out.print("Type 'X' to play with X and for '0' type anything >>> ");
 
-        if(scanner.next().toLowerCase(Locale.ROOT).equals("x")) {
-            dotHuman = DOT_X;
-            dotAi = DOT_0;
-        } else {
-            dotHuman = DOT_0;
-            dotAi = DOT_X;
-        }
-    }
 
 
     private static void aiTurn() {
         int x;
         int y;
+
+        //метод определения шансов на победу аи, если тут false, то проверяем на блок победы игрока и если там false, то аи ходит рандомно
+        checkMyChances();
+        blockWinHuman();
+
         do {
             x = random.nextInt(fieldSizeX);
             y = random.nextInt(fieldSizeY);
-        } while (!isCellValid(y, x));
+        } while (!isCellValid(y, x));   //коммент ниже по данному коду...
 
-        field[y][x] = dotAi;
+        field[y][x] = dotAi;    //коммент ниже по данному коду...
     }
     private static void humanTurn() {
         int x;
@@ -174,19 +194,30 @@ public class TicTacTou {
             System.out.print("Please enter coordinates x & y >>>> ");
             x = scanner.nextInt() - 1;
             y = scanner.nextInt() - 1;
-        } while (!isCellValid(y, x));
-
-        field[y][x] = dotHuman;
+        } while (!isCellValid(y, x));   //тут мы будем играть пока с метода isCellValid будет возвращаться true, и тут мы его конвертим
+                                        //в false, тк как только while() увидит true цикл прервется.
+        field[y][x] = dotHuman;   //устанавливаем фишку игрока после проверки на возможность установки в isCellValid(y, x)
     }
 
+
+    public static boolean checkMyChances() {
+
+        return false;
+    }
+
+    public static boolean blockWinHuman() {
+        return false;
+    }
 
     private static boolean checkWin(char dot) {
         boolean flag = false;
         int counterX = 0;
         int counterY = 0;
         int counterD = 0;
+
         //hor
         for (int y = 0; y < fieldSizeY; y++) {
+            counterX=0;
             for (int x = 0; x < fieldSizeX; x++) {
                 if (field[y][x] == dot) {
                     flag = true;
@@ -196,15 +227,17 @@ public class TicTacTou {
                 }
                 else {
                     flag = false;
-                    break;
                 }
             }
             if(counterX == fieldSizeX)
                 break;
         }
+        if(counterX == fieldSizeX)
+            return flag;
 
         //ver
         for (int y = 0; y < fieldSizeY; y++) {
+            counterY=0;
             for (int x = 0; x < fieldSizeX; x++) {
                 if (field[x][y] == dot) {
                     flag = true;
@@ -219,9 +252,12 @@ public class TicTacTou {
             if(counterY == fieldSizeY)
                 break;
         }
+        if(counterY == fieldSizeY)
+            return flag;
 
-        //diag
+        //diag слева - направо
         for (int y = 0; y < fieldSizeY; y++) {
+            counterD=0;
             for (int x = 0; x < fieldSizeX; x++) {
                 if (x == y && field[x][y] == dot) {
                     flag = true;
@@ -237,28 +273,35 @@ public class TicTacTou {
             if(counterD == fieldSizeY)
                 break;
         }
-        /*//diag 2
-        for (int x = fieldSizeX; x >= 0; ) {
+        if(counterD == fieldSizeY)
+            return flag;
+        counterD=0;
+
+        //diag 2 справа - налево
+        for (int x = fieldSizeX-1; x > 0; ) {
+            //counterD=0;
             for (int y = 0; y < fieldSizeY; y++) {
-                if (field[y][x] == dot) {
+                if (field[y][x]==dot) {
                     flag = true;
+                    counterD++;
+                    if(counterD == fieldSizeY)
+                        break;
                 }
                 else {
                     flag = false;
-                    break;
                 }
                 x--;
             }
-            if(!flag)
+            if(counterD == fieldSizeY)
                 break;
         }
-        */
-        //diag 2
-        if(field[2][0] == dot && field[1][1] == dot && field[0][2] == dot) return true;
+        if(counterD == fieldSizeY)
+            return flag;
 
-        return flag;
+        return false;
     }
 
+    //проверка - заполненна ли карта, если да, то ничья
     private static boolean checkDraw() {
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
@@ -276,9 +319,7 @@ public class TicTacTou {
         return x >= 0 && y >= 0 && x < fieldSizeX && y < fieldSizeY && field[y][x] == DOT_EMPTY;
     }
 
-
-
-
+    //инициализация карты точками
     private static void initField(int sizeX, int sizeY) {
         fieldSizeY = sizeY;
         fieldSizeX = sizeX;
@@ -293,7 +334,6 @@ public class TicTacTou {
 
     private static void printField() {
         System.out.println("+");
-
         for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
             System.out.print(i % 2 == 0 ? "-" : i / 2 + 1);
         }
