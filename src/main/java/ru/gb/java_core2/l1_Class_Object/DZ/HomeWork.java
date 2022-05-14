a331748b854ae01d8e989b8b3f9733a850f1e957
DAZZ <art.murtazov@gmail.com>
2022-05-14 13:45:07 +0300

    l1 DZ finished

--- src/main/java/ru/gb/java_core2/l1_Class_Object/DZ/HomeWork.java
+++ src/main/java/ru/gb/java_core2/l1_Class_Object/DZ/HomeWork.java
@@ -5,29 +5,27 @@ public class HomeWork {
 
     public static void main(String[] args) {
 
-        //сохздал команду
+
+        //создал команду
+        //Team team = new Team(5);
         Team[] teams = new Team[4];
         teams[0] = new Team("John");
         teams[1] = new Team("Matt");
         teams[2] = new Team("Den");
         teams[3] = new Team("Fill");
 
-        //Создал препятствия
-        Course[] course = new Course[teams.length];
+        //создал препятствия
+        Course[] course = new Course[3];
         course[0] = new Course();
         course[1] = new Course();
         course[2] = new Course();
-        course[3] = new Course();
-
 
 
         //метод вывода информации обо всех членах команды
-        System.out.println("Names of our warriors:");
-        teams[0].allTeamInfo(teams, teams.length);
-        System.out.println("______________________");
+        teams[0].TeamInfo(teams, teams.length);
 
         //метод для вывода информации о членах команды, прошедших дистанцию
-        teams[0].passCourseTeamPlayers(teams, course);
+        course[0].doIt(teams, course);
 
 
 
--- src/main/java/ru/gb/java_core2/l1_Class_Object/DZ/Team.java
+++ src/main/java/ru/gb/java_core2/l1_Class_Object/DZ/Team.java
@@ -1,19 +1,25 @@
 package ru.gb.java_core2.l1_Class_Object.DZ;
 
-import java.util.Arrays;
 
 public class Team {
 
     private final String name;
-    private int power;
-    private int difficulty;
+    private final int power;
+    //private Team[] team;
+    //private int teamCount;
 
 
     public Team(String name) {
         this.name = name;
         power = (int) (Math.random() * 100);
     }
-
+    /*
+    public Team(int teamCount) {
+        for (int i = 0; i < teamCount; i++) {
+            team[i] = new Team();
+        }
+    }
+    */
 
     public String getName() {
         return name;
@@ -21,40 +27,19 @@ public class Team {
     public int getPower() {
         return power;
     }
-
-
-
-    public void passCourseTeamPlayers(Team[] tArr, Course[] cArr) {
-        boolean flag = true;
-
-        for (int i = 0; i < tArr.length; i++) {
-            flag = true;
-            for (int j = 0; j < cArr.length; j++) {
-                if(tArr[i].power > cArr[j].getDifficulty())
-                {
-                    System.out.printf("Player %s PASSED course. Power: %d - Course: %d\n", tArr[i].getName(), tArr[i].power, cArr[j].getDifficulty());
-                }
-                else
-                {
-                    flag = false;
-                    System.out.printf("Player %s DO NOT passed the course. Power: %d - Course: %d\n", tArr[i].getName(), tArr[i].power, cArr[j].getDifficulty());
-                }
-            }
-            if(flag)
-                System.out.printf("Player %s PASSED ALL course.\n", tArr[i].getName());
-            else
-                System.out.printf("Player %s DO NOT passed ALL course.\n", tArr[i].getName());
-
-            System.out.println("_____________________\n");
-        }
+    /*
+    public Team[] getTeam() {
+        return team;
     }
+    */
 
-    public void allTeamInfo(Team[] arr, int length) {
+    public void TeamInfo(Team[] arr, int length) {
+        System.out.println("Our warriors:");
 
         for (int i = 0; i < length; i++) {
             System.out.printf("Name: %s, Power: %d\n", arr[i].getName(), arr[i].getPower());
         }
-
+        System.out.println("___________________________________________________________________________________\n");
     }
 
 
